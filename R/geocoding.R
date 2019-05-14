7ecdfe03a88254151e5ffd4900da85fb54f915cb
Jens von Bergmann <jens@mountainmath.ca>
2019-05-14 12:08:24 -0700

    fix geocoder and generalize

--- R/geocoding.R
+++ R/geocoding.R
@@ -1,22 +1,24 @@
 #' Use BC Geocoder to geocode addresses. This is slow, goes through address list one by one
 #' @param data data frame with rows to be geocoded
 #' @param address_field column in data frame that contains the address string
+#' @param localities optional array of locality names to restrict search to
 #' @return data frame with `X`, `Y` columns for longitude and latitude, as well as
 #' matching `score`, `matchPrecision` and cleaned `usedAddressString`
 #'
 #' @export
-geocode <- function(data,address_field="addressString") {
+geocode <- function(data,address_field="addressString",localities=NULL) {
   # api_key <- getOption("bc_geocoder_api_key") Not needed apparently
   base_url="https://apps.gov.bc.ca/pub/geocoder/addresses.csv"
   matchPrecision <- 'SITE, UNIT, CIVIC_NUMBER, INTERSECTION, BLOCK'
   for (i in 1:nrow(data)) {
-    if (is.na(data[i,"X"])) {
+    if ((!"X" %in% names(data)) || is.na(data[i,"X"])) {
       address_string=data[[address_field]][i]
-      address_string=paste0(sub(",$","",sub(" #\\d+.*,",",",sub(" Vancouver.*$","",address_string))),", Vancouver, BC")
+      if (!is.null(localities) && length(localities)==1 && localities[0]=="Vancouver")
+        address_string=paste0(sub(",$","",sub(" #\\d+.*,",",",sub(" Vancouver.*$","",address_string))),", Vancouver, BC")
       query=list(addressString=address_string,
-                 localities="Vancouver",
                  matchPrecision=matchPrecision,
                  provinceCode="BC")
+      if (!is.null(localities) && length(localities)>0) query["localities"]=localities
       response<-httr::GET(base_url,query=query)
       if (response$status_code==200) {
         suppressMessages(suppressWarnings(r <- readr::read_csv(response$content)))
@@ -25,6 +27,8 @@ geocode <- function(data,address_field="addressString") {
         data$score[i]=r$score
         data$matchPrecision[i]=r$matchPrecision
         data$usedAddressString[i]=address_string
+        data$fullAddress=r$fullAddress
+        data$faults=r$faults
       }
     }
     if (i %% 100 ==0 ) print(paste0("Done with ",i,"/",nrow(data)))
