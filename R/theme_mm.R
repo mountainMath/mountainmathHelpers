027a3e19994ca54a32096ceb1f18175f8a682b97
Jens von Bergmann <jens@mountainmath.ca>
2022-04-28 10:02:29 -0700

    better theme handling

--- R/theme_mm.R
+++ R/theme_mm.R
@@ -1,21 +1,21 @@
 #' MountainMath theme
 #'
-#' @param background_colour Background colour for plot and panel
 #' @param ... Additional parameters passed to theme()
+#' @param background_colour Background colour for plot and panel
 #' @return A theme to be added to a ggplot object
 #' @export
-theme_mm <- function(background_colour = "#F8F4F0",...){
+theme_mm <- function(...,background_colour = "#F8F4F0"){
   ggplot2::theme_light() +
     ggplot2::theme(plot.title.position = "plot",
                    plot.caption.position = "plot",
-                   plot.title = ggplot2::telement_text(hjust=0.5),
-                   plot.subtitle = ggplot2::telement_text(hjust=0.5),
+                   plot.title = ggplot2::element_text(hjust=0.5),
+                   plot.subtitle = ggplot2::element_text(hjust=0.5),
                    text = ggplot2::element_text(family = "Times New Roman"),
                    plot.caption = ggplot2::element_text(hjust=1),
                    plot.background = ggplot2::element_rect(fill=background_colour),  #F8F0F4
                    panel.background = ggplot2::element_rect(fill=background_colour),
                    legend.background = ggplot2::element_rect(fill=background_colour),
-                   #legend.box.background = element_rect(fill=background_colour),
+                   #legend.box.background = ggplot2::element_rect(fill=background_colour),
                    legend.key = ggplot2::element_rect(fill=background_colour),
                    panel.grid.major = ggplot2::element_line(colour="grey")) +
     ggplot2::theme(...)
